caf78155c77636e5677244f89fcee790cd73eb03
Sreenivasan V Rajangan <rvsreeni@gmail.com>
2017-11-11 21:11:47 +0530

    Add files via upload

---
+++ ACD_MDS_Python_Session#1_Assignment#2.3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Reverse a word after accepting the input from the user.
+"""
+inp_list = list(input("Enter input string: "))
+inp_list.reverse()
+print(''.join(inp_list))
